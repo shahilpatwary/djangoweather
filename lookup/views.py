530a10497ba3691b2d74ba88ba63e5eb7982c167
shahil <shahilpatwary@gmail.com>
2021-10-30 11:01:13 +0600

    API

--- lookup/views.py
+++ lookup/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 
 def home(request):
-	return render(request,'home.html', {})
+	import json
+	import requests
+
+	api_request= requests.get("https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=89129&distance=5&API_KEY=9D6B79B7-061A-4E41-B70B-8B6395EC089B")
+	try:
+		api = json.loads(api_request.content)
+
+	except Exception as e:
+		api="Error"
+	return render(request,'home.html', {'api': api})
 
 def about(request):
 	return render(request,'about.html', {})
